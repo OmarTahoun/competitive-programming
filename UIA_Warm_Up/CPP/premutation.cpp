1b5987469daab629ec1ce43e5b8facdb58965e74
OmarTahoun <omaraiz489@gmail.com>
2019-03-25 14:53:05 +0800

    more

---
+++ UIA_Warm_Up/CPP/premutation.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+	int n;
+	cin>>n;
+	if (n%2!=0)
+		cout<<-1<<endl;
+	else{
+		for (int i = 1; i <=n; ++i)
+		{
+			if (i%2!=0)
+				cout<<i+1;
+			else
+				cout<<i-1;
+			if (i<n)
+				cout<<" ";
+			else
+				cout<<endl;
+		}
+	}
+	return 0;
+}
