7360ed2f57bc790e43478573d9fe45577fd4221c
OmarTahoun <omaraiz489@gmail.com>
2019-06-16 12:30:26 +0300

    more practice

--- Code Forces/PY/Good Number.py
+++ Code Forces/PY/Good Number.py
@@ -1,10 +1,10 @@
 n, k = map(int, input().split())
 b = {str(i) for i in range(k+1)}
-print(b)
+b = set(b)
 cnt = 0
 for i in range(n):
     a = set(input())
-    print(a)
-    if a >= b: cnt += 1
+    if b.issubset(a):
+    	cnt += 1
 
 print(cnt)
